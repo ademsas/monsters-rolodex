217d91ab9904f1dda66ea46462b98242c438c80b
Tish Demsas <tish@shiphawk.com>
2020-04-07 16:12:10 -0700

    Updates

---
+++ precache-manifest.51f7abd494e1fac9ce4efaa88af37531.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef767e77125c69f8520cf36ea970f3b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "250c2f8634dfd37f2954",
+    "url": "/monsters-rolodex/static/css/main.f27bf0fe.chunk.css"
+  },
+  {
+    "revision": "530f0522b804ed93e999",
+    "url": "/monsters-rolodex/static/js/2.9c992584.chunk.js"
+  },
+  {
+    "revision": "250c2f8634dfd37f2954",
+    "url": "/monsters-rolodex/static/js/main.ab4254bf.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
